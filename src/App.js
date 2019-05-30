b56fed0f4d3f38c139d6339f1f74edba2c82ed74
Scott-Current <Scott.Current@officedepot.com>
2019-05-30 09:11:27 -0400

    changed text to current

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          RE RO RU REACT!!!
+          Hello from the Currents!
         </a>
       </header>
     </div>
